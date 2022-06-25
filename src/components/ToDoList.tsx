2418e2fbb92a8e30652d021fba0fa04fde5422eb
minnje97 <dlalswp112@naver.com>
2022-06-25 18:57:51 +0900

    220622

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
+
+interface IForm {
+  toDo: string;
+}
+
+interface IToDo {
+  id: number;
+  text: string;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
+function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const onSubmit = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category: "TO_DO" },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+  };
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("toDo", {
+            required: "Please write a to do",
+          })}
+          placeholder="Write a to do"
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
